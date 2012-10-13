4a082fd9c0995672034ba99a88c7ba7ca95f375b
Ricky Elrod <ricky@elrod.me>
2012-10-13 14:30:50 -0400

    Use Akka 2.1-M2 isntead of M1

--- project/Build.scala
+++ project/Build.scala
@@ -6,9 +6,12 @@ object MiniBCSBuild extends Build {
   val buildOptions = Defaults.defaultSettings ++ Seq(
     scalaVersion := "2.10.0-M7",
     resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
+    resolvers += "Typesafe Snapshots" at "http://repo.typesafe.com/typesafe/snapshots/",
+    resolvers += "Central" at "https://oss.sonatype.org/content/repositories/releases/",
     libraryDependencies := Seq(
-      "com.typesafe.akka" % "akka-actor" % "2.1-M1",
-      "com.typesafe.akka" % "akka-remote" % "2.1-M1",
+      // Update these to use %% when 2.10 goes live because I have no idea how to force it to use that.
+      "com.typesafe.akka" % "akka-actor_2.10.0-M7" % "2.1-M2",
+      "com.typesafe.akka" % "akka-remote_2.10.0-M7" % "2.1-M2",
       "org.joda" % "joda-convert" % "1.2",
       "joda-time" % "joda-time" % "2.1"
     ),
