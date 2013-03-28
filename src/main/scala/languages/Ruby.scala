bcd9dbd04c3bdb9ee279344492d43f36a0a7445c
Ricky Elrod <ricky@elrod.me>
2013-03-28 04:59:57 -0400

    Remove some useless imports.

--- src/main/scala/languages/Ruby.scala
+++ src/main/scala/languages/Ruby.scala
@@ -1,9 +1,6 @@
 package gd.eval.languages
 import gd.eval.SandboxedLanguage
 
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
-
 case class Ruby(code: String) extends SandboxedLanguage {
   val extension = "rb"
   val command = Seq("ruby", file.getAbsolutePath)
