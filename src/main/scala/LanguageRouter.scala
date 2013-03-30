a00d96844763353ff387063375b0c334a5fcb3b5
Ricky Elrod <ricky@elrod.me>
2013-03-30 05:03:34 -0400

    Rework Router.route - but does not break the public API.

--- src/main/scala/LanguageRouter.scala
+++ src/main/scala/LanguageRouter.scala
@@ -2,10 +2,12 @@ package gd.eval
 import gd.eval.languages._
 
 object Router {
-  def route(language: String, code: String) = language match {
-    case "ruby" | "mri" => Some(Ruby(code))
-    case "scala" => Some(Scala(code))
-    case "c" => Some(C(code))
-    case _ => None
-  }
+  /* The list of languages we can evaluate. Keep alphabetical. */
+  val languages = Map(
+    "c" -> C,
+    "ruby" -> Ruby,
+    "scala" -> Scala
+  )
+
+  def route(language: String, code: String) = languages.get(language).map(_(code))
 }
---
+++ src/test/scala/Router.scala
@@ -0,0 +1,22 @@
+package tests
+import gd.eval.Router
+import org.scalatest.{BeforeAndAfter, FunSpec, ParallelTestExecution}
+import org.scalatest.matchers.ShouldMatchers
+
+class RouterSpec
+  extends FunSpec
+  with ShouldMatchers
+  with BeforeAndAfter
+  with ParallelTestExecution {
+
+  describe("The Router") {
+    it("should be able to route") {
+      Router.route("ruby", "puts 1") should not be (None)
+      Router.route("foobar", "foo bar") should be (None)
+    }
+
+    it("should be able to give a list evaluatable languages") {
+      Router.languages.keys should contain ("ruby")
+    }
+  }
+}
