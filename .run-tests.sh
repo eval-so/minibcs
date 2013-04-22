7555b9c3e76df1b69376c23562e3052cba42c520
Ricky Elrod <ricky@elrod.me>
2013-04-22 04:44:58 -0400

    What is computer? How do I httpie?

--- .run-tests.sh
+++ .run-tests.sh
@@ -8,5 +8,5 @@ sbt update | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" > $results
 sbt test | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" >> $results
 
 statuscode="$?"
-url=`http POST https://www.refheap.com/api/paste contents=="$(cat $results)" private==true | python -c 'import json,sys;print json.load(sys.stdin)["url"]+"/raw"'`
+url=`http -f POST https://www.refheap.com/api/paste contents="$(cat $results)" private=true | python -c 'import json,sys;print json.load(sys.stdin)["url"]+"/raw"'`
 http -f POST http://rcmp.tenthbit.net/$network/$channel payload="{\"custom_ci\": 1, \"commit\": \"$(git log -1 --format=%h)\", \"branch\": \"$(git rev-parse --abbrev-ref HEAD)\", \"repository_name\": \"$repo\", \"results_url\": \"$url\", \"status\": \"$statuscode\"}"
