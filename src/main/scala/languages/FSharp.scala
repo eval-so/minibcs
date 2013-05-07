43aad3733bd70a7517fdec839853434baf16043d
Ricky Elrod <ricky@elrod.me>
2013-05-07 10:57:11 -0400

    [F#] Add nologo to nuke the copyright message on every compilation.

--- src/main/scala/languages/FSharp.scala
+++ src/main/scala/languages/FSharp.scala
@@ -4,6 +4,6 @@ import so.eval.{ EvaluationRequest, SandboxedLanguage }
 case class FSharp(evaluation: EvaluationRequest) extends SandboxedLanguage {
   val extension = "fs"
   override lazy val filename = s"program.${extension}"
-  override val compileCommand = Some(Seq("fsharpc", "-o", "a.exe", filename))
+  override val compileCommand = Some(Seq("fsharpc", "--nologo", "-o", "a.exe", filename))
   val command = Seq("mono", "a.exe")
 }
