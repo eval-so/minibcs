abeb61914963a2c5295394e58724072f3bed41fc
Ricky Elrod <ricky@elrod.me>
2013-05-17 02:46:47 -0400

    Pass -nologo to vbnc.

--- src/main/scala/languages/VBNET.scala
+++ src/main/scala/languages/VBNET.scala
@@ -4,6 +4,6 @@ import so.eval.{ EvaluationRequest, SandboxedLanguage }
 case class VBNET(evaluation: EvaluationRequest) extends SandboxedLanguage {
   val extension = "vb"
   override lazy val filename = s"program.${extension}"
-  override val compileCommand = Some(Seq("vbnc", filename))
+  override val compileCommand = Some(Seq("vbnc", "-nologo", filename))
   val command = Seq("mono", "program.exe")
 }
